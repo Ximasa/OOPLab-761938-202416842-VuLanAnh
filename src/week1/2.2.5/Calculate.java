4d1279a0db9d28aaea1521c97147a41034817538
Ximasa <vulananh150505@gmail.com>
2026-03-27 17:13:33 +0700

    week1

--- src/week1/2.2.5/Calculate.java
+++ src/week1/2.2.5/Calculate.java
@@ -1,7 +1,31 @@
 import javax.swing.JOptionPane;
 public class Calculate{
     public static void main(String[] args){
-        String num1, num2;
-        String strNotification = ""
+        String strnum1, strnum2;
+        String strNotification1 = "Sum: ";
+        String strNotification2 = "Difference: ";
+        String strNotification3 = "Product: ";
+        String strNotification4 = "Quotient: ";
+
+        strnum1 = JOptionPane.showInputDialog(null, 
+            "Please input the first double number: ", 
+            "Input the first double number", JOptionPane.INFORMATION_MESSAGE);
+        strnum2 = JOptionPane.showInputDialog(null, 
+            "Please input the second double number: ",
+            "Input the second double number", JOptionPane.INFORMATION_MESSAGE);
+        
+        double num1 = Double.parseDouble(strnum1);
+        double num2 = Double.parseDouble(strnum2);
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        JOptionPane.showMessageDialog(null, strNotification1 + sum + "\n" +
+            strNotification2 + difference + "\n" + strNotification3 + product + "\n" +
+            strNotification4 + quotient + "\n", "Results", JOptionPane.INFORMATION_MESSAGE);
+
+    System.exit(0);
     }
 }
