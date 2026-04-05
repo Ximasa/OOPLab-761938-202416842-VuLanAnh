4edd658205c332b561c4a1580c50cc60c3e9beb1
Ximasa <vulananh150505@gmail.com>
2026-04-05 15:35:39 +0700

    backup: officially save all work to draft branch

--- AimsProject/src/aims/Cart.java
+++ AimsProject/src/aims/Cart.java
@@ -1,4 +1,151 @@
 package aims;
 
 public class Cart {
+//11. Create the Cart class to work with DigitalVideoDisc
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] =
+            new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
+
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered < MAX_NUMBERS_ORDERED) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc '" + disc.getTitle() + "' has been added.\n");
+        } else {
+            System.out.println("The cart is almost full (Maximum 20 items).\n");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        int index = -1;
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i] == disc) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            for (int i = index; i < qtyOrdered - 1; i++) {
+                itemsOrdered[i] = itemsOrdered[i + 1];
+            }
+            itemsOrdered[qtyOrdered - 1] = null;
+            qtyOrdered--;
+            System.out.println("The disc has been removed.\n");
+        } else {
+            System.out.println("Disc not found in cart! Cannot remove!\n");
+        }
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+
+    public void filterByID(int id) {
+        boolean found = false;
+        System.out.println("Search Result for ID: " + id + ".\n");
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getId() == id) {
+                System.out.println(itemsOrdered[i].toString());
+                found = true;
+                break;
+            }
+        }
+        if (!found) System.out.println("No DVD found with ID: " + id + ".\n");
+    }
+
+    public void filterByTitle(String title) {
+        boolean found = false;
+        System.out.println("Search Result for Title: " + title + ".\n");
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].getTitle().toLowerCase().contains(title.toLowerCase())) {
+                System.out.println(itemsOrdered[i].toString());
+                found = true;
+            }
+        }
+        if (!found) System.out.println("No DVD found with title: " + title + ".\n");
+    }
+
+    public void sortCartbyCost() {
+        for (int i = 0; i < qtyOrdered - 1; i++) {
+            for (int j = 0; j < qtyOrdered - i - 1; j++) {
+                if (itemsOrdered[j].getCost() < itemsOrdered[j + 1].getCost()) {
+                    DigitalVideoDisc temp = itemsOrdered[j];
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                    itemsOrdered[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Cart sorted by cost (decreasing).\n");
+    }
+
+    public void sortCartbyTitle() {
+        for (int i = 0; i < qtyOrdered - 1; i++) {
+            for (int j = 0; j < qtyOrdered - i - 1; j++) {
+                if (itemsOrdered[j].getTitle().compareToIgnoreCase(itemsOrdered[j+1].getTitle()) > 0) {
+                    DigitalVideoDisc temp = itemsOrdered[j];
+                    itemsOrdered[j] = itemsOrdered[j + 1];
+                    itemsOrdered[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Cart sorted by title (alphabetical).\n");
+    }
+
+    public void chooseFreeItem() {
+        if (qtyOrdered > 0) {
+            int rd = (int) (Math.random() * qtyOrdered);
+            System.out.println("Lucky! The DVD '" + itemsOrdered[rd].getTitle() + "' is now FREE!\n");
+        }
+    }
+
+    public void placeOrder() {
+        if (qtyOrdered == 0) {
+            System.out.println("Your cart is empty. Cannot place order.\n");
+        } else {
+            System.out.println("Order created successfully!\n");
+            System.out.println("Total cost: " + totalCost() + " $\n");
+            qtyOrdered = 0;
+            itemsOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+        }
+    }
+
+    public void updateQuantity(DigitalVideoDisc disc, int qty) {
+        System.out.println("Quantity updated for: " + disc.getTitle() + ".\n");
+    }
+
+    //12. Create Carts of DigitalVideoDiscs
+    public void printCart() {
+        System.out.println("Ordered Items:");
+        for (int i = 0; i < qtyOrdered; i++) {
+            System.out.println((i + 1) + ". [ID: " + itemsOrdered[i].getId() + "] " + itemsOrdered[i].getTitle() + " : "
+                    + itemsOrdered[i].getCost() + " $\n");
+        }
+        System.out.println("Total Cost: " + totalCost() + " $\n");
+    }
+
+    //14. Working with method overloading
+    //14.1. Overloading by differing types of parameter
+    public void addDigitalVideoDisc_array(DigitalVideoDisc[] dvdList) {
+        for (DigitalVideoDisc disc : dvdList) {
+            addDigitalVideoDisc(disc);
+        }
+    }
+
+    public void addDigitalVideoDisc_varang(DigitalVideoDisc... dvds) {
+        for (DigitalVideoDisc disc : dvds) {
+            addDigitalVideoDisc(disc);
+        }
+    }
+
+    //14.2  Overloading by differing the number of parameters
+    public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
+        addDigitalVideoDisc(dvd1);
+        addDigitalVideoDisc(dvd2);
+    }
 }
--- AimsProject/src/aims/DigitalVideoDisc.java
+++ AimsProject/src/aims/DigitalVideoDisc.java
@@ -1,4 +1,86 @@
 package aims;
 
+//8. Create the DigitalVideoDisc class and its attributes
 public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+    //16. Classifier Member and Instance Member
+    private static int nbDigitalVideoDiscs = 0;
+    private int id;
+
+//9. Create accessors and mutators for the class DigitalVideoDisc
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+//10. Create Constructor method
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        //16. Classifier Member and Instance Member
+        nbDigitalVideoDiscs++;
+        this.id = nbDigitalVideoDiscs;
+
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+//15. Passing parameter
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+//16. Classifier Member and Instance Member
+    public int getId() {
+        return id;
+    }
 }
+
+/*Trả lời câu hỏi: If you create a constructor method to build a DVD by title then create a constructor method to build
+a DVD by category. Does JAVA allow you to do this?
+Câu trả lời là: Java không cho phép tạo hai hàm khởi tạo chỉ cùng nhận một tham số kiểu String như title và category.
+Lý do:
+1. Em đã thử và không thể tạo constructor method cho category sau khi đã tạo cho title trước đó,
+   chương trình báo constructor đã tồn tại.
+2. Trình biên dịch dựa vào Method Signature — gồm số lượng, kiểu dữ liệu và thứ tự tham số — để phân biệt các hàm nạp
+   chồng.
+   Vì cả hai thuộc tính đều là String, Java sẽ gặp lỗi trùng lặp chữ ký và không thể xác định giá trị truyền vào thuộc
+   về title hay category khi khởi tạo đối tượng.
+*/
