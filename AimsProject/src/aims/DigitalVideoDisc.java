d003265edcc03fee0b0e8985080ae5214c9a9edc
Ximasa <vulananh150505@gmail.com>
2026-04-05 21:23:38 +0700

    feat: initial implementation for AIMS project (Section 7-10)

---
+++ AimsProject/src/aims/DigitalVideoDisc.java
@@ -0,0 +1,56 @@
+package aims;
+
+//8. Create the DigitalVideoDisc class and its attributes
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    //9. Create accessors and mutators for the class DigitalVideoDisc
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float getCost() {
+        return cost;
+    }
+
+    //10. Create Constructor method
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.director = director;
+        this.category = category;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
