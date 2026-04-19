d1170181a5c03383d1258b60686767edee0f9dd1
Ximasa <vulananh150505@gmail.com>
2026-04-20 01:16:49 +0700

    done lab 3.14

--- AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
+++ AimsProject/src/hust/soict/hedspi/aims/media/CompactDisc.java
@@ -7,20 +7,27 @@ public class CompactDisc extends Disc { */
 public class CompactDisc extends Disc implements Playable {
 
     private String artist;
-    private ArrayList<Track> tracks = new ArrayList<Track>();
+    //private ArrayList<Track> tracks = new ArrayList<Track>();
+    private ArrayList<Track> tracks;
 
     public CompactDisc() {
         super();
+        this.tracks = new ArrayList<Track>();
     }
+
+    /*public CompactDisc() {
+        super();
+    }*/
 
     public CompactDisc(int id, String title, String category, float cost, int length, String director, String artist) {
         super(id, title, category, cost, length, director);
         this.artist = artist;
+        this.tracks = new ArrayList<Track>();
     }
 
     public String getArtist() { return artist; }
 
-    public void addTrack(Track track) {
+    /*public void addTrack(Track track) {
         if (tracks.contains(track)) {
             System.out.println("Track '" + track.getTitle() + "' is already in the CD.");
         } else {
@@ -58,5 +65,47 @@ public class CompactDisc extends Disc implements Playable {
         for (Track track : tracks) {
             track.play();
         }
+    }*/
+
+    public void addTrack(Track track) {
+        if (tracks.contains(track)) {
+            System.out.println("Track '" + track.getTitle() + "' is already in the CD.");
+        } else {
+            tracks.add(track);
+            System.out.println("Track '" + track.getTitle() + "' added successfully.");
+        }
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+            System.out.println("Track '" + track.getTitle() + "' removed.");
+        } else {
+            System.out.println("Track '" + track.getTitle() + "' does not exist.");
+        }
+    }
+
+    @Override
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+
+    @Override
+    public void play() {
+        if (this.getLength() > 0) {
+            System.out.println("Playing CD: " + this.getTitle() + " by " + this.getArtist());
+            System.out.println("CD total length: " + this.getLength());
+            System.out.println("----------");
+            for (Track track : tracks) {
+                track.play();
+            }
+        } else {
+            System.out.println("ERROR: CD length is 0 or less, cannot be played!");
+        }
     }
 }
+
