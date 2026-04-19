077e989e7f284c61d1dabeefc1f2162c7be2dab8
Ximasa <vulananh150505@gmail.com>
2026-04-20 00:30:29 +0700

    done lab 3.13

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,11 +1,13 @@
 package store;
 
 import disc.DigitalVideoDisc;
+import media.Media;
+
 import java.util.ArrayList;
 
 public class Store {
+    /*Lab 3.5
     private ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<DigitalVideoDisc>();
-
     public void addDVD(DigitalVideoDisc dvd) {
         itemsInStore.add(dvd);
         System.out.println("DVD '" + dvd.getTitle() + "' has been added to the store.");
@@ -26,5 +28,38 @@ public class Store {
             System.out.println((i + 1) + ". " + itemsInStore.get(i).toString());
         }
         System.out.println("*************************\n");
+    }*/
+
+    //Lab 3.13
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
+    public void addMedia(Media media) {
+        if (!itemsInStore.contains(media)) {
+            itemsInStore.add(media);
+            System.out.println("The media '" + media.getTitle() + "' has been added to the store.");
+        } else {
+            System.out.println("The media '" + media.getTitle() + "' is already in the store.");
+        }
+    }
+
+    public void removeMedia(Media media) {
+        if (itemsInStore.contains(media)) {
+            itemsInStore.remove(media);
+            System.out.println("The media '" + media.getTitle() + "' has been removed from the store.");
+        } else {
+            System.out.println("The media '" + media.getTitle() + "' is not found in the store.");
+        }
+    }
+
+    public void printStore() {
+        System.out.println("***********************STORE***********************");
+        System.out.println("Items in store:");
+        for (int i = 0; i < itemsInStore.size(); i++) {
+            System.out.println((i + 1) + ". " + itemsInStore.get(i).toString());
+        }
+        System.out.println("***************************************************");
+    }
+
+    public ArrayList<Media> getItemsInStore() {
+        return itemsInStore;
     }
 }
