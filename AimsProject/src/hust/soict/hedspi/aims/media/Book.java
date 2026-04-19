c81f5b4060c72549ab54bb2969176c3e84b8dd1b
Ximasa <vulananh150505@gmail.com>
2026-04-19 23:14:05 +0700

    done lab3.8

---
+++ AimsProject/src/hust/soict/hedspi/aims/media/Book.java
@@ -0,0 +1,49 @@
+package media;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    private int id;
+    private String title;
+    private String category;
+    private float cost;
+    private List<String> authors = new ArrayList<String>();
+
+    public Book(int id, String title, String category, float cost) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public float getCost() { return cost; }
+    public void setCost(float cost) { this.cost = cost; }
+
+    public void addAuthor(String authorName) {
+        if (!authors.contains(authorName)) {
+            authors.add(authorName);
+            System.out.println("Author '" + authorName + "' added successfully.");
+        } else {
+            System.out.println("Author '" + authorName + "' is already in the list.");
+        }
+    }
+
+    public void removeAuthor(String authorName) {
+        if (authors.contains(authorName)) {
+            authors.remove(authorName);
+            System.out.println("Author '" + authorName + "' removed successfully.");
+        } else {
+            System.out.println("Author '" + authorName + "' does not exist in the list.");
+        }
+    }
+}
